463c32041e5905a526dae0c9fa213b873fcb4ba8
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-11-11 21:18:20 +0530

    Made project files.. Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "prashant.h"
+#include "priyank.h"
+#include "rishabh.h"
+#include "ujjwal.h"
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ ujjwal.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+#include "ujjwal.h"
+
+using namespace std;
+
+string displayName(string name) {
+    return "Hello " + name;
+}
