8193e5060d5002dd116e1a0284a22fd849659fe2
PRIYANK MAHOUR <32241835+priyankmahour@users.noreply.github.com>
2022-11-25 15:26:52 +0530

    Update priyank.h

--- priyank.h
+++ priyank.h
@@ -1,8 +1,1 @@
-#include<bits/stdc++.h>
-void insertWordInTrie(string);
-void populateTrie(void); 
-void load_history(void);
-void save_history(void);
-void print_history(void);
-void print_history2(void);
 
