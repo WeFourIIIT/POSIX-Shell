97fd3bc68539c4f10b39febf51dbf27f1529378c
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-11-28 16:59:07 +0530

    All integrations done

--- priyank.h
+++ priyank.h
@@ -7,49 +7,11 @@ using namespace std;
 void insertWordInTrie(string);
 void populateTrie(void);
 static inline void trim(string &);
-// class TrieNode{
-//     public:
-//         bool isLeaf;
-//         string word;
-//         TrieNode() {
-//             isLeaf = false;
-//             word = "";
-//         }
-// };
-// class Trie{
-//     public:
-//         TrieNode *root;
-//         Trie();
-//         void insert(string);
-//         bool search(string);
-//         bool startsWith(string);
-//         bool isLastNode(TrieNode *);
-//         void autocompleteRecUtil(TrieNode *, string &, vector<string> &);
-//         void autocomplete(string &, vector<string> &);
-//         void autocorrectRecUtil(TrieNode *, char, string &, vector<int>, vector<pair<string, int> > &, int &);
-//         void autocorrect(string, int, vector<pair<string, int> > &);
-//         void insertWordInTrie(string);
-//         void populateTrie();
-// };
-// class his_trie{
-//     public:
-//         his_trie *ref[26];
-//         bool flag;
-//         int count;
-
-//         void insert(his_trie*, string, int);
-//         bool char_exist(char);
-//         bool check(his_trie, string);
-//         bool is_valid_check(his_trie, string, vector<pair<int, string>>);
-//         void collect_strings(vector<pair<int, string>>, his_trie*, string);
-// };
-
 class TrieNode
 {
 public:
     bool isLeaf;
     string word;
-    // TrieNode *arr[26];
     map<char, TrieNode *> children;
 
     TrieNode()
@@ -58,6 +20,7 @@ public:
         this->word = "";
     }
 };
+
 class Trie
 {
 public:
@@ -68,247 +31,81 @@ public:
         root = new TrieNode();
     }
 
-    void insert(string word) // priyank done
+    void insert(string data) // priyank done
     {
-
         TrieNode *curr = this->root;
-
-        for (int i = 0; i < word.size(); i++)
+        int lm=data.size();
+        for (int i = 0; i < lm; i++)
         {
+            if (curr->children.find(data[i]) == curr->children.end()) // if anything with char word[i] not found in map 
+                curr->children[data[i]] = new TrieNode();
 
-            int index = word[i] - 'a';
-
-            if (curr->children.find(word[i]) == curr->children.end()) // if anything with char word[i] not found in map 
-            {
-                curr->children[word[i]] = new TrieNode();
-            }
-
-            // curr = curr->arr[index];
-            curr = curr->children[word[i]];
+            curr = curr->children[data[i]];
         }
         curr->isLeaf = true;
-        curr->word = word;
+        curr->word = data;
 
-        // cout << "insertion done " << curr->word << endl;
-    }
-
-    bool search(string word)  // not found any use done priyank 
-    {
-        TrieNode *curr = this->root;
-        if (curr == nullptr)
-        {
-            return false;
-        }
-
-        for (int i = 0; i < word.size(); i++)
-        {
-
-            int index = word[i] - 'a';
-
-            // curr = curr->arr[index];
-            curr = curr->children[word[i]];
-            if (curr == nullptr)
-            {
-                return false;
-            }
-        }
-
-        return curr->isLeaf;
-    }
-
-    bool startsWith(string prefix)  // not found any use done priyank 
-    {
-
-        TrieNode *curr = this->root;
-        if (curr == nullptr)
-        {
-            return false;
-        }
-
-        for (int i = 0; i < prefix.size(); i++)
-        {
-
-            int index = prefix[i] - 'a';
-
-            // curr = curr->arr[index];
-            curr = curr->children[prefix[i]];
-
-            if (curr == nullptr)
-            {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     bool isLastNode(TrieNode *root)  // done priyank
     {
-        if (root->children.size() == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return (root->children.size() == 0);
     }
 
-    void autocompleteRecUtil(TrieNode *curr, string &currPrefix, vector<string> &res) // dfs here done priyank
+    void autocompleteRecUtil(TrieNode *curr, string &n_pre, vector<string> &op) // dfs here done priyank
     {
-        if (curr->isLeaf) {
-            res.push_back(currPrefix);
-        }
+        if (curr->isLeaf) 
+            op.push_back(n_pre);
 
-        if (isLastNode(curr)) {
+        if (isLastNode(curr))
             return;
+
+        for (auto idef = curr->children.begin(); idef != curr->children.end(); idef++) 
+        {
+            TrieNode *n_ch = idef->second;
+
+            n_pre.push_back(idef->first);
+
+            autocompleteRecUtil(n_ch, n_pre, op);
+
+            n_pre.pop_back();
         }
 
-        for (auto itr = curr->children.begin(); itr != curr->children.end(); itr++) {
-            char currentChar = itr->first;
-            TrieNode *nextChild = itr->second;
-
-            currPrefix.push_back(currentChar);
-
-            autocompleteRecUtil(nextChild, currPrefix, res);
-
-            currPrefix.pop_back();
-        }
     }
 
-    void autocomplete(string &prefix, vector<string> &res)  // done priyank 
+    void autocomplete(string &prefix, vector<string> &op)  // done priyank 
     {
         TrieNode *curr = this->root;
 
-        if (curr == nullptr)
-        {
+        if (curr == NULL)
             return;
-        }
 
-        // check if prefix itself is present or not
-        for (int i = 0; i < prefix.size(); i++)
+        //see if prefix  is there in trie
+        int lm=prefix.size();
+        for (int i = 0; i < lm; i++)
         {
-            if (curr->children[prefix[i]] == nullptr)
-            {
-                return;
-            }
-            else
-            {
+            if (curr->children[prefix[i]] != NULL)
                 curr = curr->children[prefix[i]];
-            }
+            else
+                return;
         }
 
-        // if prefix is a word add it to result
-        if (curr->isLeaf && isLastNode(curr))
-        {
-            res.push_back(prefix);
-            // MAY need to delete this statement because first line in recursive function is doing this
-            // OR may node to exactly follow gf reference.
-        }
+        // prefix is found to be valid data
+        if (isLastNode(curr) && curr->isLeaf)
+            op.push_back(prefix);
 
-        // if prefix is last node in branch
+        // prefix is valid data , but last data of trie
         if (isLastNode(curr))
         {
             return;
         }
 
-        // if prefix branch has more nodes below
-        string tempPrefix = prefix;
-        autocompleteRecUtil(curr, tempPrefix, res);
+        string n_pre = prefix;
+        autocompleteRecUtil(curr, n_pre, op);
 
         return;
     }
 
-    // END autocomplete
-
-    // BEGIN autocorrect
-
-    void autocorrectRecUtil(
-        TrieNode *curr,
-        char ch,
-        string &word,
-        vector<int> pRow,
-        vector<pair<string, int> > &res,
-        int &maxCostAllowed)
-    {
-        int cols = word.size() + 1;
-        vector<int> currentRow(1, pRow[0] + 1);
-
-        for (int i = 1; i < cols; i++)
-        {
-            int insertCost = currentRow[i - 1] + 1;
-            int deleteCost = pRow[i] + 1;
-
-            int replaceCost = -1;
-            if (word[i - 1] != ch)
-            {
-                replaceCost = pRow[i - 1] + 1;
-            }
-            else
-            {
-                replaceCost = pRow[i - 1];
-            }
-
-            int minCost = min(insertCost, min(deleteCost, replaceCost));
-            currentRow.push_back(minCost);
-        }
-
-        // if the last entry in the row indicates the optimal cost is less than the
-        // maximum cost, and there is a word in this trie node, then add it
-        int lastIndex = currentRow.size() - 1;
-        if (currentRow[lastIndex] <= maxCostAllowed && !(curr->word.empty()))
-        { // may need to consider second condition
-            res.push_back({ curr->word, currentRow[lastIndex] });
-        }
-
-        // if any entries in the row are less than the maximum cost, then
-        // recursively search each branch of the trie
-        int minCurrRow = INT_MAX;
-        for (int i = 0; i < currentRow.size(); i++)
-        {
-            minCurrRow = min(minCurrRow, currentRow[i]);
-        }
-
-        if (minCurrRow <= maxCostAllowed)
-        {
-            for (auto itr = curr->children.begin(); itr != curr->children.end(); itr++)
-            {
-                char ch = itr->first;
-                TrieNode *nextNode = itr->second;
-
-                autocorrectRecUtil(curr->children[ch], ch, word, currentRow, res, maxCostAllowed);
-            }
-        }
-    }
-
-    void autocorrect(string word, int maxCostAllowed, vector<pair<string, int> > &res)
-    {
-        // creating first row for given word, Trie is not considered at this point
-        int wl = word.size();
-        vector<int> currentRow(wl + 1);
-        for (int i = 0; i < wl + 1; i++)
-        {
-            currentRow[i] = i;
-            // cout << currentRow[i] << endl;
-        }
-
-        TrieNode *curr = this->root;
-
-        for (auto itr = curr->children.begin(); itr != curr->children.end(); itr++)
-        {
-            char ch = itr->first;
-            TrieNode *nextNode = itr->second;
-
-            autocorrectRecUtil(curr->children[ch], ch, word, currentRow, res, maxCostAllowed);
-        }
-
-        // return res; no need here becuase pass by reference
-    }
-
-    void insertWordInTrie(string word) // priyank  done   moved
-    {
-        insert(word);
-    }
 
     void populateTrie(vector<string> &path_variable) /// priyank  done    moved
     {
@@ -336,8 +133,7 @@ public:
 
         for (int i = 0; i < basicCommands.size(); i++)
         {
-            // trie->insert(basicCommands[i]);
-            insertWordInTrie(basicCommands[i]);
+            insert(basicCommands[i]);
         }
     }
     // END autocorrect
